4db944a99eb69d623d8af39c483b89f24096fb3d
therandomfool <mddenver@protonmail.com>
2020-12-12 23:07:29 -0800

    updated code to condense into function calls

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 - Used ballspeed = * -1 as argument to change speed from positive to negative on hitting height or width border to animate ball bouncing in opposite direction
 
+- Updated code to condense into functions
+
 
 ![bouncingball](img/ball.gif)
--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
-let ball =
-{
-x : 0,
-y : 0,
-xspeed : 9,
-yspeed : 3
+let ball = {
+  x: 0,
+  y: 0,
+  xspeed: 9,
+  yspeed: 3
 }
 
 function setup() {
@@ -12,19 +11,32 @@ function setup() {
 
 function draw() {
   background(0);
+  display();
+  speed();
+  move();
+}
+
+function display() {
   stroke(255);
   strokeWeight(4);
-  fill(255,0,0);
-  ellipse(ball.x, ball.y, 44 ,44);
+  fill(255, 0, 0);
+  ellipse(ball.x, ball.y, 44, 44);
+}
 
+
+
+function speed() {
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y + ball.yspeed;
+}
+
+function move() {
   if (ball.x > width || ball.x < 0) {
     ball.xspeed = ball.xspeed * -1;
   }
 
-  if (ball.y > height || ball.y <0) {
+  if (ball.y > height || ball.y < 0) {
     ball.yspeed = ball.yspeed * -1;
   }
 
-  ball.x = ball.x + ball.xspeed;
-  ball.y = ball.y + ball.yspeed;
 }
